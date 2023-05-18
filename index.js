7a788309bacb39dc06adc37cecaa9517a2768ea3
Stableti <saadbhattin.com>
2023-05-18 19:59:42 -0400

    feat(app): add calculator variables

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const pastValue = document.querySelector("[data-past-value]");
+const currentValue = document.querySelector("[data-current-value]");
+const allClear = document.querySelector("[data-all-clear]");
+const erase = document.querySelector("[data-delete]");
+const operators = Array.from(document.querySelectorAll("[data-operation]"));
+const numbers = Array.from(document.querySelectorAll("[data-number]"));
+const equals = document.querySelector("[data-equals]");
+
+let operationClicked = false;
+let currentOperand = "";
+let pastOperand = "";
+let currentOperation = null;
